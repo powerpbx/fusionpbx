78d0cd76dcb6a794e1b34257179defae6e581b66
powerpbx <admin@powerpbx.org>
2016-06-19 09:16:37 -0700

    Update ring_group_edit.php

--- app/ring_groups/ring_group_edit.php
+++ app/ring_groups/ring_group_edit.php
@@ -412,7 +412,6 @@ else {
 	if (strlen($ring_group_ringback) == 0) {
 		$ring_group_ringback = '${us-ring}';
 	}
->>>>>>> bca0d39b9614a3faa8622bf73364a3da36887954
 
 //get the ring group destination array
 	if ($action == "add") { $x = 0; $limit = 5; }
